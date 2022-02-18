8242c34a324446dbe6f0fdd0d3f1ce9be54b490b
Mateus Marques <95579801+mateusmarquesm@users.noreply.github.com>
2022-02-18 11:19:02 -0300

    Criação do código de criação das subpastas

--- DirOrgan.py
+++ DirOrgan.py
@@ -1,5 +1,6 @@
 from itertools import count
 import os
+from threading import local
 
 '''
 1 - Nome da pasta a ser criada
@@ -10,6 +11,7 @@ import os
 6 - Criação de template de organização de pastas
 7 - Edição do template'''
 
+# Analisa se existe caracteres especiais que não podem ser utilizados em nomes de pastas.
 def charcheck(txt):
     """
     Analisa se existe caracteres especiais que não podem ser utilizados em nomes de pastas.
@@ -22,41 +24,49 @@ def charcheck(txt):
                 cont += 1
         if cont >= 1:
             print('Os nomes de arquivo não podem conter nenhum dos seguintes caracteres:\n /\?:*<>|')
-            txt = input(str('Nome da pasta: '))
+            txt = str(input('Nome da pasta: '))
         else:
             break
     return txt    
 
+
+# Requisita o local onde será criada a pasta e analisa se é um local válido.
+def caminho():
+    """
+    Requisita o local onde será criada a pasta e analisa se é um local válido.
+    
+    """
+    while True:
+        local = str(input('Local de criação da pasta: '))
+        if os.path.isdir(local):
+            break
+        else:
+            print('Caminho inválido! Tente novamente.')
+    os.chdir(local)
+
+
 # Nome a ser designado para a nova pasta
 while True:
-    projeto = input(str('Nome do pasta: '))
+    projeto = str(input('Nome do pasta: '))
     pasta = charcheck(projeto)
-    confirma = input(str(f'Nome do projeto: {pasta}. Confirme [S/N] '))[0].upper()
+    confirma = str(input(f'Nome do projeto: {pasta}. Confirme [S/N] '))[0].upper()
     while True:
         if confirma not in 'SsNn':
             print('OPÇÃO INVÁLIDA! Tente novamente!')
-            confirma = input(str(f'Nome do projeto: {pasta}. Confirme [S/N] '))[0].upper()
+            confirma = str(input(f'Nome do projeto: {pasta}. Confirme [S/N] '))[0].upper()
         elif confirma in 'SsNn':
             break
     if confirma in 'Ss':
         break
 
-
-# Define o local onde a pasta será criada e valida se é um local válido
-
-while True:
-    caminho = input(str('Caminho do projeto: '))
-    if os.path.isdir(caminho):
-        print('Este caminho existe!')
-        break
-    else:
-        print('Caminho inválido! Tente novamente.')
-os.chdir(caminho)
+caminho()
 
 # Criação das pastas e subpastas
-
 try:
     os.mkdir(pasta)
 except:
     print('ERRO!')
-    
+os.chdir(pasta)
+qntsub = int(input('Quantas subastas deseja criar? '))
+for c in range(1, qntsub + 1):
+    os.mkdir(pasta + ' ' + str(c))

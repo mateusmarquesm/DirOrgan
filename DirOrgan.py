575b3b1040aaeb69614cc6564dad1bfdf11ecba6
Mateus Marques <95579801+mateusmarquesm@users.noreply.github.com>
2022-02-18 09:51:45 -0300

    Atualização do roteiro do programa

--- DirOrgan.py
+++ DirOrgan.py
@@ -4,10 +4,11 @@ import os
 '''
 1 - Nome da pasta a ser criada
 2 - Caminho onde a pasta vai ser criada
-3 - Criação das Subpastas
-4 - Separação dos arquivos por tipo
-5 - Criação de template de organização de pastas
-6 - Edição do template'''
+3 - Criação da pasta principal
+4 - Criação das Subpastas
+5 - Separação dos arquivos por tipo
+6 - Criação de template de organização de pastas
+7 - Edição do template'''
 
 def charcheck(txt):
     """

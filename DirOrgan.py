53fba0c429fb81f410cb9dea969d08838649f383
Mateus Marques <95579801+mateusmarquesm@users.noreply.github.com>
2022-04-08 10:58:18 -0300

    Atualização do status do roteiro do projeto

--- DirOrgan.py
+++ DirOrgan.py
@@ -6,9 +6,9 @@ from tkinter import ttk
 
 
 '''
-1 - Nome da pasta a ser criada
-2 - Caminho onde a pasta vai ser criada
-3 - Criação da pasta principal
+1 - Nome da pasta a ser criada (Feito)
+2 - Caminho onde a pasta vai ser criada (Feito)
+3 - Criação da pasta principal (Feito)
 4 - Criação das Subpastas
 5 - Separação dos arquivos por tipo
 6 - Criação de template de organização de pastas

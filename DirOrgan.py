25f8280c8745d6b013c2d78897227443ad5490d5
Mateus Marques <95579801+mateusmarquesm@users.noreply.github.com>
2022-04-12 10:12:22 -0300

    Atualização do código de criação de subpastas

--- DirOrgan.py
+++ DirOrgan.py
@@ -2,6 +2,7 @@ import os
 from tkinter import *
 from tkinter import filedialog
 from tkinter import ttk
+from functools import partial
 
 
 
@@ -29,6 +30,7 @@ def confirma():
 
 def caminho_pasta():
     global pasta
+    global caminho
     caminho = filedialog.askdirectory()
     os.chdir(caminho)
     pasta = entrada.get()
@@ -60,14 +62,26 @@ def cria_pasta():
             msgErro = ttk.Label(erro02, text='Os nomes de arquivo não podem conter nenhum dos seguintes caracteres:\n /\?:*<>|', style='ERROR.TLabel')
             Button(erro02, text='OK', command=erro02.destroy).pack(pady=10, side=BOTTOM)
             msgErro.pack(padx=5, pady=5)
-    
+
+def botao_subpasta(n):
+    return None
+
 def cria_subpasta():
+    global janelaSubpastas
     subpasta = entrada2.get()
+    i = 1
     try:
         os.mkdir(subpasta)
         lista_subpastas.append(entrada2.get())
         print(lista_subpastas)
-        ttk.Label(janelaSubpastas, text=f'Subpasta {subpasta} criada.', style='Dark.TLabel').pack(pady=3, side=BOTTOM)
+        painel3 = Frame(janelaSubpastas, bg='#212121')
+        painel3.pack()
+        ttk.Label(painel3, text=f'- Subpasta {subpasta} criada.', style='Dark.TLabel').pack(pady=3, side=LEFT)
+        btt_subpasta = Button(painel3, text='+', command=partial(botao_subpasta, 1))
+        btt_subpasta.pack(padx=3,   side=RIGHT)
+        BttSubpasta_id.append(btt_subpasta)
+        botao_subpasta()
+        i += 1
     except:
         if subpasta == '':
             erro01 = Toplevel()
@@ -76,12 +90,19 @@ def cria_subpasta():
             msgErro = ttk.Label(erro01, text='Digite o nome da pasta', style='ERROR.TLabel')
             msgErro.pack(padx=5, pady=10)
             Button(erro01, text='OK', command=erro01.destroy).pack(pady=10)
-        else:
+        elif subpasta in lista_subpastas:
             erro02 = Toplevel()
             erro02.title('DirOrgan')
             erro02.config(background='#212121')
-            msgErro = ttk.Label(erro02, text='Os nomes de arquivo não podem conter nenhum dos seguintes caracteres:\n /\?:*<>|', style='ERROR.TLabel')
-            Button(erro02, text='OK', command=erro02.destroy).pack(pady=10, side=BOTTOM)
+            msgErro = ttk.Label(erro02, text='Esta pasta já existe!', style='ERROR.TLabel')
+            msgErro.pack(padx=5, pady=10)
+            Button(erro02, text='OK', command=erro02.destroy).pack(pady=10)
+        else:
+            erro03 = Toplevel()
+            erro03.title('DirOrgan')
+            erro03.config(background='#212121')
+            msgErro = ttk.Label(erro03, text='Os nomes de arquivo não podem conter nenhum dos seguintes caracteres:\n /\?:*<>|', style='ERROR.TLabel')
+            Button(erro03, text='OK', command=erro03.destroy).pack(pady=10, side=BOTTOM)
             msgErro.pack(padx=5, pady=5)
 
 
@@ -108,6 +129,7 @@ def janela_subpastas():
 
 #Variáveis globais:
 lista_subpastas = []
+BttSubpasta_id = []
 
 # Janela inicial principal do programa
 janelaInicial = Tk()
